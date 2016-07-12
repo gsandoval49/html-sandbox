2edf4b8200c6d194c3cd38e3f36702227526a5f2
Giles Sandoval <gsandoval2277@gmail.com>
2016-07-12 13:27:55 -0600

    added batman image.

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-foo!
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>My Web Page</title>
+	</head>
+	<body>
+		<h1>Hi World</h1>
+		<h2>Welcome to my web page</h2>
+		<h3>This is an h3 tag</h3>
+		<p></p>
+		<ul>
+			<li>item 1</li>
+			<li>item 2</li>
+			<li>item 3</li>
+		</ul>
+		<ol>
+			<li>item 1</li>
+			<li>item 2</li>
+			<li>item 3</li>
+		</ol>
+		<div>
+			<a href="https://google.com" target="_blank">Go to google.com</a>
+		</div>
+		<img src="images/batman.jpg" alt="batman"/>
+	</body>
+</html>
