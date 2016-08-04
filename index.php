a5b2590f37f7f1caafab820450aea343ef6d01e7
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-04 09:35:37 -0600

    test with bootstrap

--- index.php
+++ index.php
@@ -1,9 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta charset="utf-8:">
+		<!-- this says render this page in latest IE rendering engine because MS sucks lol  better than IE 9 or 10-->
+		<meta http-equiv="x-ua-compatible" content="IE-edge">
+		<!-- feel free to check others i.e. ddcb website to take meta tags, the ones you need for your project ex. minimized window, not taskbar-->
+		<meta name="viewport" content="width=dvice-width, initial-scale=1">
+
+		<!-- OPTIONAL THEME HERE...IF YOU WANT :) -->
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified CSS-->
+		<!-- link tag href - path to cdn; sha384 this is new because it's a way to verify a legit url for a link; -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!--ADD YOUR CUSTOM CSS HERE; EX. fontawesome, note: put your own custom css last! -->
+		<link rel="stylesheet" href="css/style.css">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<!-- on it's way out and will be gone when bootstrap 4 is here. you'll see on older sites with older code, ex. legacy-->
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jquery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js" type="text/javascript"></script>
+
+		<!-- latest compiled and minifed java script -->
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- add your custom js files here -->
+		<script src="js/custom.js" type="text/javascript"></script>
 
 	</head>
 	<body>
+	<p>hai world</p>
 
 	</body>
 </html>
