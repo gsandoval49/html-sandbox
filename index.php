ad14e066b6d9a3eeb15e4a12ec9f9f83ce12463d
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-04 19:32:12 -0600

    redid columns

--- index.php
+++ index.php
@@ -34,28 +34,100 @@
 		<!-- add your custom js files here -->
 		<script src="js/custom.js" type="text/javascript"></script>
 
+
 	</head>
 
 
-	<body>
+	<body class="sfooter">
 
 	<!-- BEGIN HEADER -->
 	<!-- it's div within divs; notice the "col-md-12" syntax has to be correct-->
 	<!-- the column piece means this ".col-breakpoint-# of columns out of 12" that's what "col-md-12" means -->
+
+	<!--begin sticky footer-->
+	<div class="sfooter-content">
 	<header>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<h1>My bootstrap page</h1>
+
+					<!-- begin navbar -->
+					<nav class="navbar navbar-default">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">My Bootstrap Page</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="#">Home</a></li>
+								<li><a href="about-me">About Me</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Moar Links <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="#">Action</a></li>
+										<li><a href="#">Another action</a></li>
+										<li><a href="#">Something else here</a></li>
+										<li role="separator" class="divider"></li>
+										<li><a href="#">Separated link</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</nav>
+
+
 				</div>
 			</div>
 		</div>
 	</header>
 
 	<main>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3">
+					sidepanel
+				</div>
+				<div class="col-md-9">
+					content panel
+				</div>
+			</div>
 
+			<!-- 2nd row of columns-->
+			<div class="row">
+				<div class="col-md-4">
+					col 1
+				</div>
+				<div class="col-md-4">
+					col 2
+				</div>
+				<div class="col-md-4">
+					col 3
+				</div>
+			</div>
+
+		</div>
 	</main>
+	</div>
+	<!-- BEGIN FOOTER HERE -->
 	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					foot col 1
+				</div>
+				<div class="row">
+					foot col 2
+				</div>
+			</div>
+		</div>
 
 	</footer>
 
